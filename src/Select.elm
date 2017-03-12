94036b94e37e9d5a9ff74b78d25f7df7175d11b1
John Evans <lgastako@gmail.com>
2017-03-12 01:18:48 -0700

    Update to elm 0.18.

--- src/Select.elm
+++ src/Select.elm
@@ -1,12 +1,12 @@
-module Select exposing (from, from')
+module Select exposing (from, from_)
 
-{-| This module provides the `from` (and more flexible `from'`) helpers to make
+{-| This module provides the `from` (and more flexible `from_`) helpers to make
 working with `select` elements in Elm easier.
 
 [See full examples in github](https://github.com/lgastako/elm-select/blob/master/src/Main.elm)
 
 # Helpers
-@docs from, from'
+@docs from, from_
 
 -}
 
@@ -28,13 +28,13 @@ the drop down.
 -}
 from : List a -> (a -> msg) -> Html msg
 from xs msg =
-    from' xs msg toString toString
+    from_ xs msg toString toString
 
 
 {-| Convert a list of values and stringifying function for values of that type
 into a `select` element which contains as it's values the list of values.
 
-    from' [North, South, East, West] Direction toId toLabel
+    from_ [North, South, East, West] Direction toId toLabel
 
 like the `from` example above, this would produce a select element with box
 option values and labels being derived from the four cardinal directions and
@@ -44,14 +44,14 @@ derived using the `toId` and `toLabel` functions provided instead of defaulting
 to `toString` like `from`.
 
 -}
-from' : List a -> (a -> msg) -> (a -> String) -> (a -> String) -> Html msg
-from' xs msg toId toLabel =
+from_ : List a -> (a -> msg) -> (a -> String) -> (a -> String) -> Html msg
+from_ xs msg toId toLabel =
     let
         optionize x =
             option [ (value << toId) x ]
                 [ (text << toLabel) x ]
     in
-        select [ onInput (msg << makeFromString' xs) ]
+        select [ onInput (msg << makeFromString_ xs) ]
             (List.map optionize xs)
 
 
@@ -75,18 +75,18 @@ makeFromString xs =
         fromString
 
 
-makeFromString' : List a -> (String -> a)
-makeFromString' xs =
+makeFromString_ : List a -> (String -> a)
+makeFromString_ xs =
     let
         fromString =
             makeFromString xs
 
-        fromString' s =
+        fromString_ s =
             case fromString s of
                 Nothing ->
-                    Debug.crash "fromString returned Nothing in fromString'"
+                    Debug.crash "fromString returned Nothing in fromString_"
 
                 Just s ->
                     s
     in
-        fromString'
+        fromString_
